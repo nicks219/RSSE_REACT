6115209a3a03b28b6786cb01e5aa8c5470f6bfe9
Mikolay Medvedev <nick219.medvedev@gmail.com>
2022-05-03 12:46:46 +0300

    [DOTNET-1] refactoring + nullable

---
+++ Rsse.Base/Service.Models/CreateModel.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RandomSongSearchEngine.Data.DTO;
+using RandomSongSearchEngine.Data.Repository.Contracts;
+
+namespace RandomSongSearchEngine.Models;
+
+public class CreateModel
+{
+    private IServiceScope _scope { get; }
+    private ILogger<CreateModel> _logger { get; }
+
+    public CreateModel(IServiceScope serviceScope)
+    {
+        _scope = serviceScope;
+        _logger = _scope.ServiceProvider.GetRequiredService<ILogger<CreateModel>>();
+    }
+
+    public async Task<SongDto> ReadGenreListAsync()
+    {
+        await using var repo = _scope.ServiceProvider.GetRequiredService<IRepository>();
+        try
+        {
+            List<string> genreListResponse = await repo.ReadGenreListAsync();
+            return new SongDto(genreListResponse);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "[CreateModel: OnGet Error]");
+            return new SongDto() {ErrorMessageResponse = "[CreateModel: OnGet Error]"};
+        }
+    }
+
+    public async Task<SongDto> CreateSongAsync(SongDto createdSong)
+    {
+        await using var repo = _scope.ServiceProvider.GetRequiredService<IRepository>();
+        try
+        {
+            if (createdSong.SongGenres == null || string.IsNullOrEmpty(createdSong.Text)
+                                               || string.IsNullOrEmpty(createdSong.Title) ||
+                                               createdSong.SongGenres.Count == 0)
+            {
+                SongDto errorDto = await ReadGenreListAsync();
+                errorDto.ErrorMessageResponse = "[CreateModel: OnPost Error - empty data]";
+                if (!string.IsNullOrEmpty(createdSong.Text)) errorDto.TextResponse = createdSong.Text;
+                return errorDto;
+            }
+
+            int newSongId = await repo.CreateSongAsync(createdSong);
+            if (newSongId == 0)
+            {
+                SongDto errorDto = await ReadGenreListAsync();
+                errorDto.ErrorMessageResponse = "[CreateModel: OnPost Error - create unsuccessfull]";
+                errorDto.TitleResponse = "[Already Exist]";
+                return errorDto;
+            }
+
+            SongDto updatedDto = await ReadGenreListAsync();
+            List<string> updatedGenreList = updatedDto.GenreListResponse!;
+            List<string> songGenresResponse = new List<string>();
+            for (int i = 0; i < updatedGenreList.Count; i++)
+            {
+                songGenresResponse.Add("unchecked");
+            }
+
+            foreach (int i in createdSong.SongGenres)
+            {
+                songGenresResponse[i - 1] = "checked";
+            }
+
+            return new SongDto(updatedGenreList, newSongId, "", "[OK]", songGenresResponse);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "[CreateModel: OnPost Error]");
+            return new SongDto() {ErrorMessageResponse = "[CreateModel: OnPost Error]"};
+        }
+    }
+}
---
+++ Rsse.Tests/CatalogTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using RandomSongSearchEngine.Controllers;
+using RandomSongSearchEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RandomSongSearchEngine.Data.DTO;
+
+namespace MSTest;
+
+[TestClass]
+public class CatalogTest
+{
+    private const int PageSize = 10;
+    private IServiceScope? _fakeScope;
+    private CatalogModel? _catalogModel;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        FakeLoggerErrors.ExceptionMessage = "";
+        FakeLoggerErrors.LogErrorMessage = "";
+        _fakeScope = new FakeScope<CatalogModel>().ServiceScope;
+        _catalogModel = new CatalogModel(_fakeScope);
+    }
+
+    [TestMethod]
+    public async Task ShouldReadCatalogPageTest()
+    {
+        var response = await _catalogModel!.ReadCatalogPageAsync(1);
+        // не знаю, что должен проверять этот тест, количество песен на странице ?
+        Assert.AreEqual( /*PageSize*/response.SongsCount, response.CatalogPage?.Count);
+    }
+
+    [TestMethod]
+    public async Task ShouldNavigateTest()
+    {
+        var request = new CatalogDto() {NavigationButtons = new List<int>() {1}, PageNumber = 1};
+        var response = await _catalogModel!.NavigateCatalogAsync(request);
+        // не знаю, что должен проверять этот тест, количество песен на странице ?
+        Assert.AreEqual( /*PageSize*/response.SongsCount, response.CatalogPage?.Count);
+    }
+
+    [TestMethod]
+    public async Task IfWtfShouldLoggingErrorInsideModelTest()
+    {
+        var frontRequest = new CatalogDto() {NavigationButtons = new List<int>() {1000, 2000}};
+        var result = await _catalogModel!.NavigateCatalogAsync(frontRequest);
+        //Assert.ThrowsException<Exception>(async () => await result);
+
+        Assert.AreEqual("[CatalogModel: OnPost Error]", result.ErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task IfNullShouldLoggingErrorTest()
+    {
+        _ = await _catalogModel!.NavigateCatalogAsync(null!);
+        Assert.AreEqual("[CatalogModel: OnPost Error]", FakeLoggerErrors.LogErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task IfWtfShouldResponseZeroSongsTest()
+    {
+        var response = await _catalogModel!.DeleteSongAsync(-300, -200);
+        Assert.AreEqual(0, response.SongsCount);
+    }
+
+    [TestMethod]
+    public async Task MockThrowsExceptionInsideControllerTest()
+    {
+        var mockLogger = Substitute.For<ILogger<CatalogController>>();
+        var fakeServiceScopeFactory = Substitute.For<IServiceScopeFactory>();
+        fakeServiceScopeFactory.When(s => s.CreateScope()).Do(i => throw new Exception());
+        var catalogController = new CatalogController(fakeServiceScopeFactory, mockLogger);
+
+        _ = await catalogController.NavigateCatalogAsync(null!);
+
+        mockLogger.Received().LogError(Arg.Any<Exception>(), "[CatalogController: OnPost Error]");
+    }
+
+    [TestMethod]
+    public async Task MockShouldResponseEmptyTitleTest()
+    {
+        var mockLogger = Substitute.For<ILogger<CatalogController>>();
+        var fakeServiceScopeFactory = Substitute.For<IServiceScopeFactory>();
+        fakeServiceScopeFactory.CreateScope().Returns(_fakeScope);
+        var catalogController = new CatalogController(fakeServiceScopeFactory, mockLogger);
+
+        var response = (await catalogController.OnDeleteSongAsync(-300, -200)).Value;
+
+        Assert.AreEqual(null, response?.CatalogPage);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        _fakeScope?.Dispose();
+    }
+}
